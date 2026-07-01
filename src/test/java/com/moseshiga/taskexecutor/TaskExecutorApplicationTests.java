11dcde100e89ebed51e4267c407e4567d68842b1
Kirill Khlynovskiy <kir200699@gmail.com>
2026-07-01 07:51:59 +0300

    Initialize task executor project

---
+++ src/test/java/com/moseshiga/taskexecutor/TaskExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moseshiga.taskexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskExecutorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
